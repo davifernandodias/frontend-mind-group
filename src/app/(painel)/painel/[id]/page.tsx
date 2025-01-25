bd11fb099d5c75fdc2bd809ffd07f879cd41883f
Davi Dias <davifernandosousadias18@gmail.com>
2025-01-25 19:56:45 -0300

    feat(dashboard): added component dashboard with fake data

---
+++ src/app/(painel)/painel/[id]/page.tsx
@@ -0,0 +1,33 @@
+import CardProducts from "@/components/ui/card-products";
+import Graphics from "@/components/Graphics";
+
+export default function Page() {
+  const products = [
+    { name: "Produto 1", description: "Descrição do produto 1", price: 59.99 },
+    { name: "Produto 2", description: "Descrição do produto 2", price: 99.99 },
+    { name: "Produto 3", description: "Descrição do produto 3", price: 149.99 },
+  ];
+
+  return (
+      <div className="container mx-auto p-6">
+        {/* Linha de Gráficos */}
+        <Graphics />
+
+          <p className="text-center font-semibold text-2xl -mt-5">Em alta</p>
+        {/* Linha de Cards dos Produtos */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+
+          {products.map((product, index) => (
+            <>
+            <CardProducts
+              key={index}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+              />
+            </>
+          ))}
+        </div>
+      </div>
+  );
+}
