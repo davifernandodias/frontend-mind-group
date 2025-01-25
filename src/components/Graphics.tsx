ca79b74735b871b24452ae5c91197eaf2863438b
Davi Dias <davifernandosousadias18@gmail.com>
2025-01-25 19:58:08 -0300

    feat(chart): create component to chart pie and bar

---
+++ src/components/Graphics.tsx
@@ -0,0 +1,23 @@
+import { BarCharData } from "./ui/bar-char";
+import { PieCharData } from "./ui/pie-char";
+
+
+export default function Graphics() {
+  return (
+        <div className="container mx-auto  p-4">
+          {/* Linha de Gráficos */}
+          <div className="flex flex-col lg:flex-row  gap-4 mb-6">
+            {/* Gráfico de Pizza */}
+            <div className="flex-1 bg-white dark:bg-gray-800 rounded-lg p-2 shadow">
+
+              <PieCharData />
+            </div>
+
+            {/* Gráfico de Barras */}
+            <div className="flex-1 bg-white dark:bg-gray-800 rounded-lg p-2 shadow">
+              <BarCharData />
+            </div>
+          </div>
+        </div>
+)
+}
