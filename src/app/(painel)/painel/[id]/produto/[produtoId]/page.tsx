d2bf860e6df75936ec37b571f0c5f0c83b211450
Davi Dias <davifernandosousadias18@gmail.com>
2025-01-25 21:55:32 -0300

    feat(product): added modal for product removal confirmation

--- src/app/(painel)/painel/[id]/produto/[produtoId]/page.tsx
+++ src/app/(painel)/painel/[id]/produto/[produtoId]/page.tsx
@@ -1,9 +1,40 @@
-import { auth } from "@clerk/nextjs/server";
+"use client";
 
+import CardProducts from "@/components/ui/card-products";
+import { Button } from "@/components/ui/button";
+import ModalConfirmRemove from "@/components/ui/modal-confirm-remove"; 
+import { useState } from "react";
 
-export default async function page() {
-  const { userId } = await auth();
+export default function ProductPage() {
+  const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false);
+
   return (
-    <div>produto id : {userId}</div>
-  )
+    <div className="flex flex-col items-center p-6 space-y-6 bg-gray-50 rounded-md shadow-md max-w-2xl mx-auto">
+      <h1 className="text-2xl font-bold text-gray-800">Editar Produto</h1>
+
+      <CardProducts description="Descrição do produto" name="Machado" price={2000} />
+
+      <div className="flex space-x-4">
+        {/* Botão de Editar */}
+        <Button variant="default" className="bg-blue-600 text-white hover:bg-blue-700">
+          Editar
+        </Button>
+
+        {/* Botão de Deletar */}
+        <Button
+          variant="destructive"
+          className="bg-red-600 text-white hover:bg-red-700"
+          onClick={() => setDeleteDialogOpen(true)}
+        >
+          Deletar
+        </Button>
+      </div>
+
+      {/* Modal de confirmação de exclusão */}
+      <ModalConfirmRemove
+        isDeleteDialogOpen={isDeleteDialogOpen}
+        setDeleteDialogOpen={setDeleteDialogOpen}
+      />
+    </div>
+  );
 }
