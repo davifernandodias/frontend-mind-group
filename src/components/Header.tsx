f676f39fb6a282fd967dc099a197a9ab293a8db5
Davi Dias <davifernandosousadias18@gmail.com>
2025-01-25 21:58:05 -0300

    fix(card): assigning seeIsactive args to manage whether the button is visible or not

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import { auth } from "@clerk/nextjs/server";
 import { ProductBarHeader } from "./ui/header-collapse-products";
 import { ModeToggle } from "./ui/modeToggle";
 import ProfileIconClerk from "./ui/profile-icon-clerk";
+import Link from "next/link";
 
-
-export function DashboardHeader() {
+export async function DashboardHeader() {
+  const { userId } = await auth();
   return (
     <header className="flex items-center justify-between px-6 py-4 h-auto bg-zinc-50 dark:bg-stone-900">
       {/* Profile Icon */}
@@ -11,7 +13,13 @@ export function DashboardHeader() {
 
       {/* Center Section */}
       <div className="flex flex-1 items-center justify-center gap-4 text-neutral-800 dark:text-neutral-100 font-medium">
-        <p className="text-sm">Painel</p>
+        <Link href={`/painel/${userId}`}>
+          <p className="text-sm text-gray-700 relative group">
+            Painel
+            <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-primary-color transition-all duration-500 group-hover:w-full"></span>
+          </p>
+        </Link>
+
         <span className="text-neutral-400">/</span>
         <ProductBarHeader />
       </div>
--- src/components/ui/card-products.tsx
+++ src/components/ui/card-products.tsx
@@ -1,13 +1,16 @@
-  import { Card, CardContent, CardHeader, CardTitle } from "./card";
-  import { Button } from "./button";
+import { Card, CardContent, CardHeader, CardTitle } from "./card";
+import { Button } from "./button";
+import Link from "next/link";
 
   interface CardProductsProps {
-    name: string; // Nome do produto
-    description: string; // Descrição do produto
-    price: number; // Preço do produto
+    name: string; 
+    description: string; 
+    price: number; 
+    seeIsActive?: boolean;
   }
 
-  export default function CardProducts({ name, description, price }: CardProductsProps) {
+  export default  function CardProducts({ name, description, price, seeIsActive }: CardProductsProps) {
+
     return (
       <Card className="hover:shadow-lg hover:scale-105 transform transition duration-300">
         <CardHeader>
@@ -16,7 +19,7 @@
         <CardContent>
           <p className="text-gray-600 dark:text-gray-400">{description}</p>
           <p className="mt-2 font-bold text-gray-800 dark:text-gray-200">R$ {price.toFixed(2)}</p>
-          <Button className="mt-4">Ver</Button>
+          { seeIsActive &&  <Button className="mt-4"><Link href={"/"}>Ver</Link></Button> }
         </CardContent>
       </Card>
     );
