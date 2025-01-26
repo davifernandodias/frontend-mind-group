fd5d0e5f2d4d88e37e06b1af05c4e92746068997
Davi Dias <davifernandosousadias18@gmail.com>
2025-01-25 21:54:00 -0300

    fix(product): added component for crud operation in products

--- src/app/(painel)/painel/[id]/page.tsx
+++ src/app/(painel)/painel/[id]/page.tsx
@@ -24,6 +24,7 @@ export default function Page() {
               name={product.name}
               description={product.description}
               price={product.price}
+              seeIsActive={true}
               />
             </>
           ))}
--- src/app/(painel)/painel/[id]/produto/page.tsx
+++ src/app/(painel)/painel/[id]/produto/page.tsx
@@ -1,24 +1,30 @@
+
 import ProductsPage from "@/templates/products";
 import UserDataTable from "./datatable/_components/users-datatable";
+import FormCreateProduct from "@/components/FormCreateProduct";
 
 async function fetchUsers() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   return response.json();
 }
 
-export default async function page() {
-  const users = await fetchUsers()
+export default async function Page() {
+  const users = await fetchUsers();
 
   return (
     <ProductsPage>
-      <div>
-    <h1>
-        produtos
+      <div className="flex flex-col lg:flex-row lg:space-x-6 space-y-6 lg:space-y-0 p-6 ">
+        {/* Formulário */}
+      <FormCreateProduct />
 
-    </h1>
-    <UserDataTable users={users} /> 
+        {/* Tabela */}
+
+        <div className="flex-1">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 dark:text-white">Produtos</h1>
+          <UserDataTable users={users} />
+        </div>
       </div>
-
+      
     </ProductsPage>
-  )
+  );
 }
