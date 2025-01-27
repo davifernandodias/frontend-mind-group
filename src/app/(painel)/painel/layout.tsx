318bddfef30059cc1a9705513ae2caf5c183b300
Davi Dias <davifernandosousadias18@gmail.com>
2025-01-27 11:26:08 -0300

    feat(auth): added protected route check if token and id exists in local storage

--- src/app/(painel)/painel/layout.tsx
+++ src/app/(painel)/painel/layout.tsx
@@ -1,14 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import DashboardPage from "@/templates/dashboard";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
-export default function layout({ children }: LayoutProps) {
+
+export default function PrivateLayout({ children }: LayoutProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+
+    if (!token || !user) {
+      router.push("/");  
+    }
+  }, [router]);
+
   return (
     <>
-  <DashboardPage>
-    {children}
-  </DashboardPage>
+      <DashboardPage>
+        {children}
+      </DashboardPage>
     </>
-  )
+  );
 }
